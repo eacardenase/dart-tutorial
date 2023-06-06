ebbe2c8907191dbd0c73a7ac972012a0fa073361
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-06 10:42:45 -0500

    feat: variables and basic datatypes

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,24 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
 void main() {
-  /*
-  This is 
-  a multiline
-  comments
-  */
+  // variables
 
-  /// Documentation comment
+  // <datatype> <variableName> = value;
+  int firstValue = 19;
+  int secondValue = 5;
+  String myString = 'My String';
+  bool myBoolean = true;
+  dynamic myDynamicVariable = false; // could be of any type
+  String multilineString = '''
+    
+    My
+    Multiline
+    String
+  ''';
 
-  print('Hello World');
+  myString = 'My new String';
+
+  print(multilineString);
 
   runApp(
     const MaterialApp(
