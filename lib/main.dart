6f28498598d5cc4c5ffb3c3947b47e06f0354196
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-06 11:08:06 -0500

    feat: final vs const

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
 void main() {
-  // variables
+  // runtime constant
+  final DateTime someValue2 = DateTime.now();
 
-  // type inference
-  var someValue1 = 10;
-  final someValue2 = 10; // final variables can only be set one
-  const someValue3 = 10; // value will be known at compile time and won't change
+  // compile time constant
+  const DateTime someValue3 = DateTime
+      .now(); // Error: Const variables must be initialized with a constant value.
+
+  /*
+  DateTime.now() value is known at runtime, but const is for variables
+  which values are known at compile time.
+  */
 
   runApp(
     const MaterialApp(
