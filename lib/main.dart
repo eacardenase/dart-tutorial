e06e38dd3cc6d56c7303517d1b07dda19eda18dc
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-05 21:26:09 -0500

    feat: multiline and documentation comments

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
 void main() {
+  /*
+  This is 
+  a multiline
+  comments
+  */
+
+  /// Documentation comment
+
   print('Hello World');
 
   runApp(
