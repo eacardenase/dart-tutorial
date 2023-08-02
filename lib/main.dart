2133cae692b40472ad0d90416d3c02c00a3b39e9
Edwin Cardenas <eacardenase@unal.edu.co>
2023-08-02 18:28:21 -0500

    feat: iterables, lists and sets

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,24 @@ import 'home_screen.dart';
 void main() {
   // data types
 
-  final pokemon = <String, dynamic>{
-    'name': 'Ditto',
-    'hp': 100,
-    'isAlive': true,
-    'abilities': <String>['impostor'],
-    'sprites': <int, String>{
-      1: 'ditto/front.png',
-      2: 'ditto/back.png',
-    }
-  };
+  final numbers = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 9, 10];
 
-  print(pokemon);
-  print('Name: ${pokemon['name']}');
-  print('Front sprite: ${pokemon['sprites'][1]}');
-  print('Back sprite: ${pokemon['sprites'][2]}');
+  print('List original: $numbers');
+  print('Length: ${numbers.length}');
+  print('Index 0: ${numbers[0]}');
+  print('First: ${numbers.first}');
+  print('Last: ${numbers.last}');
+
+  final reversedNumbers = numbers.reversed;
+
+  print('Iterable: $reversedNumbers'); // returns iterable
+  print('To List: ${numbers.reversed.toList()}');
+  print('To Set: ${numbers.reversed.toSet()}');
+
+  final numbersGreaterThanFive =
+      numbers.where((number) => number > 5); // returns iterable
+
+  print('> 5: ${numbersGreaterThanFive.toSet()}');
 
   runApp(
     const MaterialApp(
