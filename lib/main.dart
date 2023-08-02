70cf2aa707ae0861f824348e0ec276a028f71e85
Edwin Cardenas <eacardenase@unal.edu.co>
2023-08-02 17:57:11 -0500

    feat: maps

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,21 @@ import 'home_screen.dart';
 void main() {
   // data types
 
-  final String pokemon = 'Ditto';
-  final int hp = 100;
-  final bool isAlive = true;
-  final List<String> abilities = ['impostor'];
-  final sprites = <String>['ditto/front.png', 'ditto/back.png'];
+  final pokemon = <String, dynamic>{
+    'name': 'Ditto',
+    'hp': 100,
+    'isAlive': true,
+    'abilities': <String>['impostor'],
+    'sprites': <int, String>{
+      1: 'ditto/front.png',
+      2: 'ditto/back.png',
+    }
+  };
 
-  // dynamic == null by default
-  dynamic errorMessage = 'Hola';
-  errorMessage = true;
-  errorMessage = [1, 2, 3, 4, 5, 6];
-  errorMessage = {1, 2, 3, 4, 5, 6};
-  errorMessage = () => false;
-  errorMessage = null;
-
-  print("""
-  $pokemon
-  $hp
-  $isAlive
-  $abilities
-  $sprites
-  $errorMessage
-""");
+  print(pokemon);
+  print('Name: ${pokemon['name']}');
+  print('Front sprite: ${pokemon['sprites'][1]}');
+  print('Back sprite: ${pokemon['sprites'][2]}');
 
   runApp(
     const MaterialApp(
