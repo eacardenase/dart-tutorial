e351fc60211b9da373e240f86e1a21f821722fbf
Edwin Cardenas <eacardenase@gmail.com>
2024-02-24 12:39:16 -0500

    feat: strings basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,30 @@
 import 'package:flutter/material.dart';
 
-import 'home_screen.dart';
-
 void main() {
-  // data types
+  // var myName = 'Edwin';
+  late final String myName;
 
-  final numbers = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 9, 10];
+  myName = 'Edwin';
 
-  print('List original: $numbers');
-  print('Length: ${numbers.length}');
-  print('Index 0: ${numbers[0]}');
-  print('First: ${numbers.first}');
-  print('Last: ${numbers.last}');
-
-  final reversedNumbers = numbers.reversed;
-
-  print('Iterable: $reversedNumbers'); // returns iterable
-  print('To List: ${numbers.reversed.toList()}');
-  print('To Set: ${numbers.reversed.toSet()}');
-
-  final numbersGreaterThanFive =
-      numbers.where((number) => number > 5); // returns iterable
-
-  print('> 5: ${numbersGreaterThanFive.toSet()}');
+  print('Hola, mi nombre es $myName.');
+  print('Hola, mi nombre es ${myName.toUpperCase()}.');
+  print('Hola, mi nombre es ${myName.toLowerCase()}.');
 
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: HomeScreen(),
+        backgroundColor: Colors.cyan,
+        body: Center(
+          child: Text(
+            'Holi :3',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     ),
   );
