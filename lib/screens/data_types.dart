87fcefc5d4a7368b8e7840d57a29b27d430b925d
Edwin Cardenas <eacardenase@gmail.com>
2024-02-24 13:30:42 -0500

    feat: dynamic type

--- lib/screens/data_types.dart
+++ lib/screens/data_types.dart
@@ -12,12 +12,23 @@ class DataTypes extends StatelessWidget {
     final List<String> abilities = ['impostor'];
     final sprites = <String>['ditto/front.png', 'ditto/back.png'];
 
+    // dynamic is null by default, so therefore is an optional value
+    dynamic errorMessage = 'Hola';
+    errorMessage = true;
+    errorMessage = [1, 2, 3];
+    errorMessage = {1, 2, 3};
+    errorMessage = () => true; // closure
+    errorMessage = null;
+
+    // errorMessage += 1; // throws error without the compiler noticing it
+
     print("""
 $pokemon
 $hp
 $isAlive
 $abilities
 $sprites
+$errorMessage
 """);
 
     return const Text('Data Types');
