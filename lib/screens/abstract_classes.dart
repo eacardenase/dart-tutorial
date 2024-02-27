bd76486b6c63a0c5b7b63077c998c8e8a862060d
Edwin Cardenas <eacardenase@gmail.com>
2024-02-27 08:49:51 -0500

    feat: class extension

--- lib/screens/abstract_classes.dart
+++ lib/screens/abstract_classes.dart
@@ -14,11 +14,39 @@ abstract class EnergyPlant {
   void consumeEnergy(double amount);
 }
 
+class WindPlant extends EnergyPlant {
+  WindPlant({
+    required double initialEnergy,
+  }) : super(energyLeft: initialEnergy, type: PlantType.wind);
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
+}
+
 class AbstractClasses extends StatelessWidget {
   const AbstractClasses({super.key});
 
+  double chargePhone(EnergyPlant plant) {
+    if (plant.energyLeft < 10) {
+      throw Exception('Not enough energy');
+    }
+
+    plant.consumeEnergy(10);
+
+    return plant.energyLeft;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final windPlant = WindPlant(initialEnergy: 100);
+
+    chargePhone(windPlant);
+
+    print(windPlant);
+    print(windPlant.energyLeft);
+    print(windPlant.type);
     return const Text('Abstract Classes');
   }
 }
